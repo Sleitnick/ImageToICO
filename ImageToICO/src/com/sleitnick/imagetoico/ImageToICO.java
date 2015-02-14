ed188215ccaa453d57f74da4662d1f34899666e0
Stephen Leitnick <sleitnick@gmail.com>
2015-02-14 13:17:36 -0500

    FIrst working version

--- ImageToICO/src/com/sleitnick/imagetoico/ImageToICO.java
+++ ImageToICO/src/com/sleitnick/imagetoico/ImageToICO.java
@@ -6,8 +6,10 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javafx.application.Application;
@@ -17,17 +19,22 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
@@ -38,8 +45,18 @@ import net.sf.image4j.codec.ico.ICOEncoder;
 
 public class ImageToICO extends Application {
 	
+	private static final int[] ICON_SIZES = {
+		16, 32, 48, 64, 96, 128, 192,
+		256, 512, -1
+	};
+	
 	private Stage primaryStage;
-	private Label testLabel;
+	private Label fileChosen;
+	private Button convertButton;
+	private File selectedFile = null;
+	private FileChooser icoSave;
+	
+	private CheckBox[] checkboxSizes = new CheckBox[ICON_SIZES.length];
 	
 	/**
 	 * Cast an {@link Image} to a {@link BufferedImage}
@@ -86,50 +103,134 @@ public class ImageToICO extends Application {
 		return images;
 	}
 	
-	public ImageToICO() {
-		/*
+	private void setSelectedFile(File file, boolean allowNull) {
+		if (file == null && !allowNull) {
+			return;
+		}
+		selectedFile = file;
+		if (file == null) {
+			fileChosen.setText("");
+		} else {
+			fileChosen.setText(selectedFile.getAbsolutePath());
+		}
+		for (CheckBox cb : checkboxSizes) {
+			cb.setVisible(file != null);
+		}
+		for (int i = 0; i < checkboxSizes.length; i++) {
+			CheckBox cb = checkboxSizes[i];
+			int size = ICON_SIZES[i];
+			cb.setVisible(file != null);
+			if (size == -1 && file != null) {
+				Dimension imgSize = getImageFileSize(file);
+				cb.setText("Original Size (" + imgSize.width + "x" + imgSize.height + ")");
+			}
+		}
+		convertButton.setVisible(file != null);
+	}
+	
+	private void convertAndSave(File file, Dimension... sizes) {
 		BufferedImage image = null;
-		BufferedImage[] images;
 		try {
-			image = ImageIO.read(new File("imgs/addfolder.png"));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		images = resizeImage(image, new Dimension(32, 32), new Dimension(64, 64));
-		File file = new File("imgs/test.ico");
-		if (!file.exists()) {
-			try {
-				file.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("Writing ICO image...");
-		try {
-			//ICOEncoder.write(image, file);
-			List<BufferedImage> imagesList = new ArrayList<BufferedImage>();
-			for (BufferedImage img : images) {
-				imagesList.add(img);
-			}
-			ICOEncoder.write(imagesList, file);
+			image = ImageIO.read(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("ICO image written");
-		Desktop desktop = Desktop.getDesktop();
-		if (desktop.isSupported(Desktop.Action.OPEN)) {
+		icoSave.setInitialDirectory(file.getParentFile());
+		{
+			String initName = file.getName();
+			if (initName.contains(".")) {
+				initName = initName.substring(0, initName.lastIndexOf("."));
+			}
+			icoSave.setInitialFileName(initName);
+		}
+		File saveFile = icoSave.showSaveDialog(primaryStage);
+		BufferedImage images[] = resizeImage(image, sizes);
+		if (saveFile != null) {
+			if (!saveFile.getName().endsWith(".ico")) {
+				saveFile = new File(saveFile.getAbsolutePath() + ".ico");
+			}
+		}
+		if (saveFile == null) {
+			
+		} else {
 			try {
-				desktop.open(file);
+				saveFile.createNewFile();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			List<BufferedImage> imagesList = Arrays.asList(images);
+			try {
+				FileOutputStream out = new FileOutputStream(saveFile);
+				ICOEncoder.write(imagesList, out);
+				out.close();
+				out.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (Desktop.isDesktopSupported()) {
+				Desktop desktop = Desktop.getDesktop();
+				if (desktop.isSupported(Desktop.Action.OPEN)) {
+					try {
+						desktop.open(saveFile);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
-		*/
+	}
+	
+	private Dimension getImageFileSize(File file) {
+		javafx.scene.image.Image img = new javafx.scene.image.Image("file:" + file.getPath());
+		Dimension size = new Dimension((int)img.getWidth(), (int)img.getHeight());
+		return size;
+	}
+	
+	private Dimension[] getSelectedDimensions() {
+		List<Dimension> dimensions = new ArrayList<Dimension>();
+		for (int i = 0; i < checkboxSizes.length; i++) {
+			CheckBox cb = checkboxSizes[i];
+			if (cb.isSelected()) {
+				int size = ICON_SIZES[i];
+				if (size == -1) {
+					if (selectedFile != null && selectedFile.exists()) {
+						Dimension s = getImageFileSize(selectedFile);
+						dimensions.add(new Dimension(s.width, s.height));
+					}
+				} else {
+					dimensions.add(new Dimension(size, size));
+				}
+			}
+		}
+		Dimension[] selectedDimensions = new Dimension[dimensions.size()];
+		dimensions.toArray(selectedDimensions);
+		return selectedDimensions;
 	}
 	
 	private void createGui() {
 		primaryStage.setTitle("Image To ICO");
 		primaryStage.setResizable(false);
+		
+		FileChooser fileChooser = new FileChooser();
+		{
+			File initDir = new File(System.getProperty("user.home"));
+			List<String> dirFiles = Arrays.asList(initDir.list());
+			if (dirFiles.contains("Pictures")) {
+				initDir = new File(initDir, "Pictures");
+			} else if (dirFiles.contains("Documents")) {
+				initDir = new File(initDir, "Documents");
+			}
+			fileChooser.setInitialDirectory(initDir);
+			fileChooser.getExtensionFilters().addAll(
+					new ExtensionFilter("All Images", "*.png", "*.jpg", "*.jpeg", "*.bmp")
+			);
+		}
+		
+		icoSave = new FileChooser();
+		icoSave.setInitialDirectory(fileChooser.getInitialDirectory());
+		icoSave.getExtensionFilters().addAll(
+				new ExtensionFilter("Icon Image", "*.ico")
+		);
 		
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
@@ -142,40 +243,61 @@ public class ImageToICO extends Application {
 		scenetitle.setId("welcome-text");
 		grid.add(scenetitle, 0, 0, 2, 1);
 		
-		Label userName = new Label("User Name:");
-		grid.add(userName, 0, 1);
-
-		TextField userTextField = new TextField();
-		grid.add(userTextField, 1, 1);
-
-		Label pw = new Label("Password:");
-		grid.add(pw, 0, 2);
-
-		PasswordField pwBox = new PasswordField();
-		grid.add(pwBox, 1, 2);
-		
-		Button btn = new Button("Sign in");
+		Button chooseImageButton = new Button("Choose Image...");
 		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 4);
+		hbBtn.setAlignment(Pos.CENTER);
+		hbBtn.getChildren().add(chooseImageButton);
+		grid.add(hbBtn, 0, 2, 2, 2);
 		
-		final Text actiontarget = new Text();
-		actiontarget.setFill(Color.FIREBRICK);
-		actiontarget.setId("actiontarget");
-        grid.add(actiontarget, 1, 6);
+		fileChosen = new Label("This is a test");
+		fileChosen.setAlignment(Pos.CENTER);
+		fileChosen.setTextAlignment(TextAlignment.CENTER);
+		grid.add(fileChosen, 0, 4, 2, 4);
+		
+		convertButton = new Button("Convert");
+		convertButton.setVisible(false);
+		HBox hbBtn2 = new HBox(10);
+		hbBtn2.setAlignment(Pos.CENTER);
+		hbBtn2.getChildren().add(convertButton);
+		grid.add(hbBtn2, 0, 11, 2, 12);
+		
+		for (int i = 0; i < ICON_SIZES.length; i++) {
+			int iconSize = ICON_SIZES[i];
+			CheckBox cb = new CheckBox(iconSize == -1 ? "Original Size" : iconSize + "x" + iconSize);
+			int column = (i >= ICON_SIZES.length / 2 ? 1 : 0);
+			int row = (column == 0 ? (6 + i) : 6 + (i - (ICON_SIZES.length / 2)));
+			grid.add(cb, column, row + 2);
+			checkboxSizes[i] = cb;
+			cb.setVisible(false);
+			if (iconSize == -1) {
+				cb.setSelected(true);
+			}
+		}
         
-        btn.setOnAction((ActionEvent actionEvent) -> {
-            actiontarget.setFill(Color.FIREBRICK);
-            actiontarget.setText("Sign in button pressed");
+		chooseImageButton.setOnAction((ActionEvent actionEvent) -> {
+        	File file = fileChooser.showOpenDialog(primaryStage);
+        	setSelectedFile(file, false);
         });
+		
+		convertButton.setOnAction((ActionEvent actionEvent) -> {
+			if (selectedFile != null) {
+				convertAndSave(selectedFile, getSelectedDimensions());
+			}
+		});
         
-        Scene loginScene = new Scene(grid, 400, 300);
+        Scene loginScene = new Scene(grid, 400, 350);
 		
 		primaryStage.setScene(loginScene);
 		primaryStage.getIcons().add(new javafx.scene.image.Image("file:imgs/icon.png"));
         loginScene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        
+        setSelectedFile(null, true);
+        
 		primaryStage.show();
+	}
+	
+	public ImageToICO() {
+		
 	}
 	
 	public static void main(String[] args) {
